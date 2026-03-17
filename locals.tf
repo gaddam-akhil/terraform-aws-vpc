d5f775e68c0866efac1f0ef9dc7278bc903a8503
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-18 00:54:26 +0530

    terraform-modules

--- locals.tf
+++ locals.tf
@@ -17,7 +17,8 @@ locals {
         {
             Name = "${var.project}-${var.environment}"
         },
-           var.igw_tags,
-   AZ_names = slice(data.aws_availability_zones.available.names , 0, 2 )
+           var.igw_tags
   )
+   AZ_names = slice(data.aws_availability_zones.available.names , 0, 2 )
+  
 } 
