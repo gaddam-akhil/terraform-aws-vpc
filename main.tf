99e349b905400a4d5d3a16db8097af1f1fdc8ff1
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-18 00:58:14 +0530

    terraform-modules

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "public" {
  count = length(var.public_subnet_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidr[count.index]
-  aws_availability_zone = local.AZ_names[count.index]
+  availability_zone = local.AZ_names[count.index]
   map_public_ip_on_launch = true 
   # to get public address
 
