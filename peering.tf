1b1930538a62bde6c840424512bbe36d65cf7a12
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-19 00:52:47 +0530

    terraform-modules

--- peering.tf
+++ peering.tf
@@ -24,4 +24,32 @@ resource "aws_vpc_peering_connection" "default" {
             Name = "${var.project}-${var.environment}-default"
         }
   )
+
+resource "aws_route" "public" {
+  count = var.Is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private" {
+  count = var.Is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database" {
+  count = var.Is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "default" {
+  count = var.Is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
 }
