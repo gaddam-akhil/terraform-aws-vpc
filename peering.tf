36262a7df96d9c5fb5aa0cede738bf2b1d0487c3
gaddam akhil <gaddamakhil900@gmail.com>
2026-03-19 00:01:57 +0530

    terraform-modules

--- peering.tf
+++ peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "defauklt" {
   
   # Acceptor
-  peer_vpc_id   = aws_vpc.default.id
+  peer_vpc_id   = data.aws_vpc.default.id
 
   # Requester
   vpc_id        = aws_vpc.main.id 
